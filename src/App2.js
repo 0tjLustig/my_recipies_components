ef14d1dcbc4cb6a89ea8b8547cdea4ae859a889e
0tjLustig <55846769+0tjLustig@users.noreply.github.com>
2021-04-19 10:19:00 -0500

    Added Navbar.js to project, need to render on index.js, created .js page from making display card

---
+++ src/App2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ImageSlider from "./Components/ImageSlider";
+import {SliderData} from "./Components/SliderData";
+import './App.css';
+
+function App2() {
+  return <ImageSlider slides={SliderData} />;
+}
+
+export default App2
+
