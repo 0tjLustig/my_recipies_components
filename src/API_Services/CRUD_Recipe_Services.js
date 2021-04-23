fcc604d33030278ad04531a89f1ce61101b4cce6
0tjLustig <55846769+0tjLustig@users.noreply.github.com>
2021-04-22 22:59:55 -0500

    Removed Unused Navbar.js

--- src/API_Services/CRUD_Recipe_Services.js
+++ src/API_Services/CRUD_Recipe_Services.js
@@ -2,7 +2,10 @@ import api from './api'
 
 class RecipeDataService {
     getAll() {
-        return api.get("http://localhost:5000/api/recipes");
+        //return api.get("https://d4d2fc41-be82-4747-ae2d-fadae5ea9d19.mock.pstmn.io")
+        api.get("https://d4d2fc41-be82-4747-ae2d-fadae5ea9d19.mock.pstmn.io/http://localhost:5000/api/recipes/").then((response) =>{
+            console.log(JSON.stringify(response.data));
+        });
     }
 
     get(name) {
